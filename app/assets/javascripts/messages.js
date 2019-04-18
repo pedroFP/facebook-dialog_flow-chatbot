fd287caac864f7915bc9b03357678b752b172613
pedroFP <pedro.araya.labra@gmail.com>
2019-04-18 02:15:14 -0400

    Scroll down automatically when send message

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,4 +2,7 @@ $(document).on('turbolinks:load', function() {
   $('.hide-chat-box').click(function(){
           $('.chat-content').slideToggle();
   });
+
+  var messageBody = document.querySelector('.chats');
+  messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
 });
